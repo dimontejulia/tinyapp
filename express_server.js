66c01dfe5eb2eeb6c9a7a83c3c4a9a0af4df3270
Julia Di Monte <dimontejulia@gmail.com>
2020-11-09 23:18:46 +0000

    added body parser to express_server

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
