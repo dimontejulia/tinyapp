1e030ccb3afe23eb7a133b132e93bc4467a00258
Julia Di Monte <dimontejulia@gmail.com>
2020-11-09 19:46:33 +0000

    using HTML in our response

--- express_server.js
+++ express_server.js
@@ -17,6 +17,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//use HTML code to pring Hello World with "world" bolded
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
