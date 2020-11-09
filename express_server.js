d8a75f371550a70955c5897c61e141edb568b58c
Julia Di Monte <dimontejulia@gmail.com>
2020-11-09 22:09:10 +0000

    added urls_index.ejs file & installed ejs

--- express_server.js
+++ express_server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-//set the view engine to ejs
-app.set("view engine", "ejs");
-
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+//set the view engine to ejs
+app.set("view engine", "ejs");
 
 //homepage: displays hello
 app.get("/", (req, res) => {
@@ -16,8 +16,13 @@ app.get("/", (req, res) => {
 });
 
 //displays JSON string representing the entire urlDatabase object
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 //use HTML code to pring Hello World with "world" bolded
