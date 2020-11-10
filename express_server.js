f9041cb483a1e7e96d04fa563fc23dfb56b600a8
Julia Di Monte <dimontejulia@gmail.com>
2020-11-10 17:50:25 +0000

    delete url added

--- express_server.js
+++ express_server.js
@@ -80,6 +80,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
